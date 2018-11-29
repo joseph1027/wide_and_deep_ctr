e470d92df162bb1c5e2e760db302db09384a1e83
Joseph Leslie Hsueh <joseph102712080104@gmail.com>
2018-11-29 18:18:51 +0800

    Update deep_embedding.py

--- deep_embedding.py
+++ deep_embedding.py
@@ -15,8 +15,8 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import one_hot
 from keras.layers.embeddings import Embedding
 
-batch_size = 102400
-data_size = 10000000
+batch_size = 10240
+data_size = 1000000
 
 def manual_shuffle(x,z,y):
 	total_len = len(y)
@@ -47,12 +47,16 @@ def read_train_data(f,batch_size):
 		for j in range(2,len(one_row_data_arr)):
 			a = one_row_data_arr[j].split(":")
 			feature_id = (a[0])
-			feature_list = feature_list+feature_id+' '
+			feature_list+= feature_id+' '
 		x.append(feature_list)
 	x,z,y = manual_shuffle(x,z,y)
 	#sm = to_sparse(x,z)
-	encoded_x = [one_hot(d,100000) for d in x]
+	#encoded_x = []
+	#for e in x:
+	#	encoded_x.append([one_hot(d,2000000) for d in e])
+	encoded_x = [one_hot(d,2000000) for d in x]
 	padded_x = pad_sequences(encoded_x,maxlen=40,padding='post')
+	#padded_x = padded_x.tolist()
 	y_ca = keras.utils.to_categorical(y,2)
 	#global cw
 	#cw = compute_weight(y_ca)
@@ -74,7 +78,8 @@ def read_test_data():
 			for j in range(2,len(one_row_data_arr)):
 				a = one_row_data_arr[j].split(":")
 				feature_id = (a[0])
-				feature_list = feature_list+feature_id+' '
+				feature_list += feature_id+' '
+				#feature_list.append(feature_id)
 			x_test.append(feature_list)
 		#Then read another half of data, most of them are "0"!
 	with open('/home/joseph/文件/prune_data/test_out100_1.txt') as ff:
@@ -87,14 +92,20 @@ def read_test_data():
 			for j in range(2,len(one_row_data_arr)):
 				a = one_row_data_arr[j].split(":")
 				feature_id = (a[0])
-				feature_list = feature_list+feature_id+' '
+				feature_list += feature_id+' '
+				#feature_list.append(feature_id)
 			x_test.append(feature_list)
-	#print(x_test[0])
-	encoded_x_test = [one_hot(d,100000) for d in x_test]
-	padded_x_test = pad_sequences(encoded_x_test,maxlen=40,padding='post')
+	print(x_test[0])
+	#encoded_x_test = []
 	#for e in x_test:
-	#	encoded_x = [one_hot(d,2000000)for d in e]
-	#	encoded_x_test.append(encoded_x)
+	#	encoded_x_test.append([one_hot(d,2000000) for d in e])
+	encoded_x_test = [one_hot(d,2000000) for d in x_test]
+	print(encoded_x_test[0])
+	padded_x_test = pad_sequences(encoded_x_test,maxlen=40,padding='post')
+	#padded_x_test = padded_x_test.tolist()
+	print(padded_x_test[0])
+	#print(padded_x_test)
+	print(type(padded_x_test))
 	#sm_test = to_sparse(x_test,z_test)
 	y_test_ca = keras.utils.to_categorical(y_test,2)
 	return padded_x_test,y_test_ca,y_test
@@ -118,27 +129,32 @@ def to_sparse(f_id,z_label):
 
 def get_model():
 	input1 = Input(batch_shape =(None,40),sparse = False)
-	embedded_layer = Embedding(input_dim=100000,output_dim=100,input_length=40)(input1)
+	embedded_layer = Embedding(input_dim=2000000,output_dim=6,input_length=40,mask_zero=False)(input1)
 	flatten_layer = Flatten()(embedded_layer)
-	hidden = Dense(32,activation='relu')(flatten_layer)
-	out = Dense(2,activation = 'softmax',kernel_initializer='random_uniform')(hidden)#,W_regularizer=regularizers.l2(0.002)
+	hidden = Dense(64,activation='relu')(flatten_layer)
+	hidden1 = Dense(32,activation='relu')(hidden)
+	hidden2 = Dense(16,activation='relu')(hidden1)
+	out = Dense(2,activation = 'softmax',kernel_initializer='uniform')(hidden2)#,W_regularizer=regularizers.l2(0.002)
 	model = Model(inputs=input1, outputs = out)
-	adam = keras.optimizers.Adam(lr=0.00001,beta_1=0.9,beta_2=0.999,epsilon=1e-08, decay=0.0)
+	adam = keras.optimizers.Adam(lr=0.000005,beta_1=0.9,beta_2=0.999,epsilon=1e-08, decay=0.0)
 	model.compile(loss = 'categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 	model.summary()
 	return model
 
 def mygenerator(batch_size,data_size):
 	accumulation=0
+	f = open('/home/joseph/文件/prune_data/train_out100_100w.txt')
 	while True:
-		with open('/home/joseph/文件/prune_data/train_out100.txt') as f:
-			if((accumulation+batch_size)<data_size):
-				padded_x,y_ca=read_train_data(f,batch_size)
-				accumulation += batch_size
-			else:
-				padded_x,y_ca=read_train_data(f,data_size-accumulation)
-				accumulation = 0
-			yield padded_x,y_ca
+		if((accumulation+batch_size)<data_size):
+			padded_x,y_ca=read_train_data(f,batch_size)
+			accumulation += batch_size
+		else:
+			padded_x,y_ca=read_train_data(f,data_size-accumulation)
+			accumulation = 0
+			f.close()
+			f = open('/home/joseph/文件/prune_data/train_out100_100w.txt')
+		yield padded_x,y_ca
+	f.close()
 
 class roc_callback(Callback):
 	def __init__(self,testing_data):
@@ -169,7 +185,7 @@ class roc_callback(Callback):
 
 def compute_weight(data_size):
 	yy = []
-	with open('/home/joseph/文件/all_data_before_prune/train.yzx.txt') as t:
+	with open('/home/joseph/文件/prune_data/train_out100.txt') as t:
 		for i in range(data_size):
 			line_data = t.readline().split()
 			yy.append(line_data[0])
@@ -182,7 +198,7 @@ def compute_weight(data_size):
 if __name__ == "__main__":
 	padded_x_test,y_test_ca,y_test = read_test_data()
 	model = get_model()
-	early_stopping = EarlyStopping(monitor='val_loss', patience=0, verbose=1, mode='auto')
+	early_stopping = EarlyStopping(monitor='val_loss', patience=1, verbose=1, mode='auto')
 	cw = compute_weight(data_size)
 	history = model.fit_generator(mygenerator(batch_size,data_size),steps_per_epoch=int(data_size/batch_size)+1,epochs=1000,verbose=1,validation_data=(padded_x_test,y_test_ca),class_weight=cw,callbacks=[roc_callback(testing_data=(padded_x_test,y_test_ca)),early_stopping])
 	print(cw)
